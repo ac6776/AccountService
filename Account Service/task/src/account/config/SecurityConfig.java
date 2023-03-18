8fa3c1cf23e4e602a50d6035a86212753f3380ff
Oleg P <ac6776@outlook.com>
2023-03-18 13:14:03 +0500

    all clear

--- Account Service/task/src/account/config/SecurityConfig.java
+++ Account Service/task/src/account/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package account.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,35 +8,36 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.servlet.http.HttpServlet;
-
 @Configuration
 public class SecurityConfig {
-
-    @Autowired
     private UserDetailsService userService;
-
-    @Autowired
     private PasswordEncoder passwordEncoder;
-
-    @Autowired
     private AuthenticationEntryPoint restAuthenticationEntryPoint;
 
     @Autowired
-    @Qualifier("delegatedAuthenticationEntryPoint")
-    private AuthenticationEntryPoint entryPoint;
+    public void setUserService(UserDetailsService userService) {
+        this.userService = userService;
+    }
+
+    @Autowired
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Autowired
+    public void setRestAuthenticationEntryPoint(AuthenticationEntryPoint restAuthenticationEntryPoint) {
+        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .httpBasic()
                     .authenticationEntryPoint(restAuthenticationEntryPoint)
-//                    .authenticationEntryPoint(entryPoint)
                 .and()
                 .csrf().disable().headers().frameOptions().disable()
                 .and()
